c292f698d0822fec2107b8aeeb83dfd7ba140271
liyiran <liy@worldticket.net>
2018-11-10 14:56:40 -0800

    bootstrap done

--- final/src/App.vue
+++ final/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container">
+        <img src="./assets/logo.jpg"/>
+        <BorrowReturnBarChart/>
+        <BorrowReturnStackMap/>
+        <StationDotMap/>
+        <UsagePieChart/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import BorrowReturnBarChart from './components/BorrowReturnBarChart'
+    import BorrowReturnStackMap from './components/BorrowReturnStackMap'
+    import StationDotMap from './components/StationDotMap'
+    import UsagePieChart from './components/UsagePieChart'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            BorrowReturnBarChart,
+            BorrowReturnStackMap,
+            StationDotMap,
+            UsagePieChart
+        }
+    }
 </script>
 
+<style lang="scss">
+    @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ final/src/components/BorrowReturnBarChart.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <p>山寨1</p>
+            </div>
+        </div>
+    </div>
+</template>
