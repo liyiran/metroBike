ae0367836607ac3d90d0814d5a9d0e520915e6c5
ningxiner2018 <ningxiner2018@users.noreply.github.com>
2018-11-28 10:27:55 -0800

    app.vue

--- final/src/App.vue
+++ final/src/App.vue
@@ -20,7 +20,7 @@
     import StationMapBox from './components/StationMapBox'
     import UsagePieChart from './components/UsagePieChart'
     import AverageMonthLineChart from './components/AverageMonthLineChart'
-    import countByRegion from './components/countByRegion'
+    import CountByRegion from './components/CountByRegion'
     export default {
         name: 'app',
         components: {
@@ -33,7 +33,7 @@
 
             AverageMonthLineChart,
 
-            countByRegion
+            CountByRegion
 
         }
     }
