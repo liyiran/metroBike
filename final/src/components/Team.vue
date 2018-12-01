f531e83c3cda94d00e8fb03aa316d79256858134
liyiran <liy@worldticket.net>
2018-11-30 21:20:27 -0800

    Add team view

---
+++ final/src/components/Team.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>Yiran Li, Yueqin Yang, Xiner Ning</h3>
+        <img src="../assets/Team-Eureka.jpg" class="img-fluid" alt="Responsive image">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Team"
+    }
+</script>
+
+<style scoped>
+
+</style>
