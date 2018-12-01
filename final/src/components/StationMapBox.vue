642899bd52803d24a7111539bdff514d018ca81f
liyiran <liy@worldticket.net>
2018-11-30 20:12:35 -0800

    fix resize error

--- final/src/components/StationMapBox.vue
+++ final/src/components/StationMapBox.vue
@@ -223,7 +223,13 @@
         },
         mounted: function () {
             this.drawMap();
+            // window.addEventListener('resize', this.onResize)
+        },
+        ready: function () {
             window.addEventListener('resize', this.onResize)
+        },
+        beforeDestroy: function () {
+            window.removeEventListener('resize', this.onResize)
         }
     }
 </script>
