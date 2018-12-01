83e854196b878dd000e7a7dafa315f8e8a37cbbe
liyiran <liy@worldticket.net>
2018-11-30 21:20:27 -0800

    Add team view

--- final/src/components/Team.vue
+++ final/src/components/Team.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        <h3>Yiran Li, Yueqin Yang, Xiner Ning</h3>
+        <div class="row">
+            <div class="col">
+                <h3 align="center">Yiran Li</h3>
+                <h4 align="center">liyiran at usc.edu</h4>
+            </div>
+            <div class="col">
+                <h3 align="center">Yueqin Yang</h3>
+                <h4 align="center">Yueqinya at usc.edu</h4>
+            </div>
+            <div class="col">
+                <h3 align="center">Xiner Ning</h3>
+                <h4 align="center">XinerNin at usc.edu</h4>
+            </div>
+        </div>
         <img src="../assets/Team-Eureka.jpg" class="img-fluid" alt="Responsive image">
     </div>
 </template>
